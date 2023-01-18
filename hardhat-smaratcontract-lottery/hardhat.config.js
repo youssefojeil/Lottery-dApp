aa8b9b1deeb75415b954d9a8a627b3fd1b724e5a
unknown <youssef.ojeil@hotmail.com>
2023-01-17 19:44:11 -0500

    compile smart contract with complete chainlink vrf functionality

--- hardhat-smaratcontract-lottery/hardhat.config.js
+++ hardhat-smaratcontract-lottery/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
-require("hardhat/deploy");
+//require("hardhat/deploy");
 require("solidity-coverage");
 require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
